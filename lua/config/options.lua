bd03de43d98ef5e99d271f84fa0ca6012caad72c
c1rew <cholyonzh@gmail.com>
2023-10-08 14:41:36 +0800

    *update config, add im-select plugin.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,14 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Tab
+vim.opt.tabstop = 4 -- number of visual spaces per TAB
+vim.opt.softtabstop = 4 -- number of spacesin tab when editing
+vim.opt.shiftwidth = 4 -- insert 4 spaces on a tab
+vim.opt.expandtab = true -- tabs are spaces
+
+-- 显示不可见字符的显示
+vim.opt.list = true
+-- 行尾空格显示为一个点，tab显示为 ▸·
+vim.opt.listchars = { trail = "·", tab = "▸·" }
